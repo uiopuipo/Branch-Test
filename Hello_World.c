72a5a70a22b7054c66e81389189ecceec6818299
JKS <rltjr1948@kpu.ac.kr>
2017-11-10 12:04:24 +0900

    added somelines

--- Hello_World.c
+++ Hello_World.c
@@ -6,5 +6,9 @@ int main(void)
 	printf("HelloHello!\n");
 	printf("GoodByeGoodBye!\n");
 	printf("GoodBye World!\n");
+	printf("Hello World!\n");
+	printf("HelloHello!\n");
+	printf("GoodByeGoodBye!\n");
+	printf("GoodBye World!\n");
 	return 0;
 }
